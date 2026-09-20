c744cf615083e42c63bea33a9772801c3fe26fb5
YuOkyn <damascenojoaovictor02@gmail.com>
2026-09-19 21:45:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let home = document.getElementById("mainpage")
+let aba = document.getElementById("aba")
+function openaba(){
+    home.style.width = "70%"
+    aba.style.width = "30%"
+}
+function closeaba(){
+    home.style.width = "100%"
+    aba.style.width = "0%"
+}
